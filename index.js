a557f88318f8868b688647e558b9b5ad0d1214a8
Zoe Monet Williams <mwilliamszoe@gmail.com>
2018-08-10 19:39:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,11 @@ return 'I love you, too.'
 }
 sayHiToGrandma("I love you, Grandma.")
 
-function sayHiToGrandma(string){
-return 'I love you, too.'
-}
-sayHiToGrandma("I love you, Grandma.")
+function sayHiToGrandma(string) {
+ if (string.toLowerCase() === string) {
+ return "I can’t hear you!”
+} if (string.toUpperCase() === string ) {
+ return "YES INDEED!”
+} if (string === `I love you, Grandma.` ) {
+ return "I love you, too.”
+}}
